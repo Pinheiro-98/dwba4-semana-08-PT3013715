c005eccb335f28b262519df8ba215ac7df779d9d
Juliana Silva <julianasilva180198@gmail.com>
2022-09-23 00:19:23 +0000

    Initial commit

---
+++ qst_quatro.js
@@ -0,0 +1,10 @@
+class Aluno {
+  contructor (primeiroNome, segundoNome, primeiraNota, segundaNota){
+    this.primeiroNome = nome;
+    this.segundoNome = sobrenome;
+    this.primeiraNota = nt1;
+    this.segundaNota = nt2;
+  }
+  get getcompleto[]{
+    return this.nomeCompleto()
+  }
---
+++ qst_tres.js
@@ -0,0 +1,23 @@
+var nome = "João da Silva";
+var nt_1 = 7;
+var nt_2 = 8.5;
+var final = (nt_1 * 0.6) + (nt_2 * 0.4);
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + nt_1 + "\nNota2: " + nt_2);
+alert("Nome do aluno: " + nome + "\nNota final: " + final);
+
+var nome = "Maria Fernandes Santos";
+var nt_1 = 9;
+var nt_2 = 3.4;
+var final = (nt_1 * 0.6) + (nt_2 * 0.4);
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + nt_1 + "\nNota2: " + nt_2);
+alert("Nome do aluno: " + nome + "\nNota final: " + final);
+
+var nome = "Cassia Monteiro";
+var nt_1 = 4;
+var nt_2 = 9.8;
+var final = (nt_1 * 0.6) + (nt_2 * 0.4);
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + nt_1 + "\nNota2: " + nt_2);
+alert("Nome do aluno: " + nome + "\nNota final: " + final);
